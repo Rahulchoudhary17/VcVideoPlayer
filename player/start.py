78501e4e414b47967696208ea3ce3810e3070550
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2021-09-03 10:14:59 +0530

    Update start.py

--- player/start.py
+++ player/start.py
@@ -4,12 +4,12 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       await m.reply(f"**Hey, I'm a VC Video Player developed by Developers of** @SiderzBot \n\n**To use it:-** __ \n1) Add this Bot to your Group and Make it Admin \n2) Add__ @SiderzVideoPlayer __to your Group__ \n3) **Commands** : \n`/stream` (IN REPLY TO A VIDEO) \n`/stop`",   
+       await m.reply(f"**Hey, I'm a VC Video Player developed by Developers of** @MLO_EMPIRE \n\n**To use it:-** __ \n1) Add this Bot to your Group and Make it Admin \n2) Add__ @MLo_VidEoPlayer __to your Group__ \n3) **Commands** : \n`/stream` (IN REPLY TO A VIDEO) \n`/stop`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                      InlineKeyboardButton(
-                                            "Support", url="t.me/SiderzChat")
+                                            "Support", url="t.me/MLO_EMPIRE")
                                     ]]
                             ))
    else:
-      await m.reply("**SiderzStreamBot is Alive! ✨**")
+      await m.reply("**MLo_VidEoPlayer is Alive! ✨**")
